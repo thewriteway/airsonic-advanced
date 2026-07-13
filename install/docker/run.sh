cf7c16751952c0a1bb746c04baf467fb02f853b2
chosenpath <13290180+thewriteway@users.noreply.github.com>
2026-07-13 08:59:48 +0100

    Increase Java Heap size in docker
    Increase Java Heap size in docker

--- install/docker/run.sh
+++ install/docker/run.sh
@@ -20,7 +20,7 @@ if [[ $# -lt 1 ]] || [[ ! "$1" == "java"* ]]; then
     while IFS= read -r -d '' item; do
         java_opts_array+=( "$item" )
     done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
-    exec java -Xmx256m \
+    exec java -Xmx512m \
      -cp /app/WEB-INF/classes:/app/WEB-INF/lib/*:/app/WEB-INF/lib-provided/* \
      -Dserver.address=0.0.0.0 \
      -Dserver.port=$AIRSONIC_PORT \
